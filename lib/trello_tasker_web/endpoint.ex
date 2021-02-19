c9cf49c0b7e3977aeec198a847f52c860b686b06
thvitti <thvittix@gmail.com>
2021-02-19 15:38:46 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/trello_tasker_web/endpoint.ex
+++ lib/trello_tasker_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule TrelloTaskerWeb.Endpoint do
   ]
 
   socket "/socket", TrelloTaskerWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
